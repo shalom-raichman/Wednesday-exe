fa4c20988edca158e8f1bf83a987e2d090e63ece
Shalom Raichman <shalomr1770@gmail.com>
2024-09-11 16:36:33 +0300

    add exe-4

--- index.js
+++ index.js
@@ -31,7 +31,51 @@ const capitalizeFirstLetter = (str) => {
     return newStr
 }
 
+
+// object of tasks
+const tasks = [
+    {
+    task1: {
+      id: '1',  // Unique ID
+      details: 'Complete the project report',
+      time: '2024-09-11 10:00'
+    },
+    task2: {
+      id: '2',  // Unique ID
+      details: 'Attend the team meeting',
+      time: '2024-09-11 14:00'
+    },
+    task3: {
+      id: '3',  // Unique ID
+      details: 'Submit code review',
+      time: '2024-09-11 16:00'
+    }
+  }
+];
+
+console.log(tasks);
+
+  
+
 // exe-4
-const saveTasks = () => {
-    
+const saveTasks = (tasks) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    // return true
 }
+
+const LocalStorage = () => {
+    return JSON.parse(localStorage.getItem("tasks")) || []
+}
+
+const addTask = (task) => {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || []
+    tasks.push(task);
+    localStorage.tasks = JSON.stringify(tasks);
+}
+
+const removeTask = (id) => {
+    const tasks = JSON.parse(localStorage.getItem("tasks")).filter(t => t.id != id) || []
+    localStorage.tasks = JSON.stringify(tasks);
+}
+
+saveTasks(tasks);
