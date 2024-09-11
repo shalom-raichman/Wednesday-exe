a4be5b0e3c802d33d3a39403409dbfac1c5c7a15
Shalom Raichman <shalomr1770@gmail.com>
2024-09-11 15:54:27 +0300

    comlited exe-3

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const arr = [4, 5, 8, 9, 2, 2, 6]
+const arr = [4, 5, 8, 9, 2, 2, 6];
+const str = "Shalom i'm grate.";
 
+// exe-1
 const evenUp = (arr) => {
     return arr.filter(e => e %2 == 0).sort();
 }
 
+// exe-2
 const noDoual = (arr) => {
-    const newArr = []
+    const newArr = [];
     for (const el of arr) {
         if (!newArr.includes(el)) {
-            newArr.push(el)
+            newArr.push(el);
         }
     }
     return newArr
 }
 
-console.log(noDoual(arr));
+// exe-3
+const uperCase = (str) => {
+    const strArr = str.split(" ");
+    let newStr = ""
+    for (let word of strArr) {
+        if(word.endsWith(".")){
+            newStr += word
+            continue
+        }
+        newStr += word[0].toLocaleUpperCase() + word.slice(1)  + " ";
+    }
+}
 
+uperCase(str)
